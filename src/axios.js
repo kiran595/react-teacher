b29c01a7b5e24c1cab57c03d2aaaf85b52225e52
illusionist <nikhil.shrestha1995@gmail.com>
2019-02-11 16:08:12 +0545

    updated form

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://6bekckwd2k.execute-api.us-east-2.amazonaws.com/dev'
+    baseURL: 'https://test-boocamp-123.firebaseio.com/'
 })
 
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
--- src/containers/NewTeacher/NewTeacher.js
+++ src/containers/NewTeacher/NewTeacher.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Container, Form, Button, FormGroup, Input } from "reactstrap";
 
 import axios from "axios";
 
@@ -15,7 +16,8 @@ class NewTeacher extends Component {
     console.log(this.props);
   }
 
-  postDataHandler = () => {
+  postDataHandler = e => {
+    e.preventDefault();
     const data = {
       email: this.state.email,
       firstname: this.state.firstname,
@@ -24,72 +26,68 @@ class NewTeacher extends Component {
       description: this.state.description
     };
 
-    axios
-      .post("/teacher", data)
-      .then(response => console.log(response));
+    console.log(data);
+
+    axios.post("https://test-boocamp-123.firebaseio.com/teacher.json", data).then(response => console.log(response));
   };
 
   render() {
     return (
-      <div className="container">
-          <div className="form-group">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              value={this.state.email}
-              onChange={event => this.setState({ email: event.target.value })}
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-group">
-            <label>FirstName</label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.firstname}
-              onChange={event =>
-                this.setState({ firstname: event.target.value })
-              }
-              placeholder="Password"
-            />
-          </div>
-          <div className="form-group">
-            <label>LastName</label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.lastname}
-              onChange={event =>
-                this.setState({ lastname: event.target.value })
-              }
-            />
-          </div>
-          <div className="form-group">
-            <label>City</label>
-            <input
-              type="text"
-              class="form-control"
-              value={this.state.city}
-              onChange={event => this.setState({ city: event.target.value })}
-            />
-          </div>
+      <div>
+        <Container>
+          <Form onSubmit={this.postDataHandler}>
+            <FormGroup>
+              <label>Email address</label>
+              <Input
+                type="email"
+                value={this.state.email}
+                onChange={event => this.setState({ email: event.target.value })}
+                placeholder="Enter email"
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>FirstName</label>
+              <Input
+                type="text"
+                value={this.state.firstname}
+                onChange={event =>
+                  this.setState({ firstname: event.target.value })
+                }
+                placeholder="Enter First Name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>LastName</label>
+              <Input
+                type="text"
+                value={this.state.lastname}
+                onChange={event =>
+                  this.setState({ lastname: event.target.value })
+                }
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>City</label>
+              <Input
+                type="text"
+                value={this.state.city}
+                onChange={event => this.setState({ city: event.target.value })}
+              />
+            </FormGroup>
 
-          <div className="form-group">
-            <label>Description</label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.description}
-              onChange={event =>
-                this.setState({ description: event.target.value })
-              }
-            />
-          </div>
-          <button className="btn btn-primary" onClick={this.postDataHandler}>
-            Submit
-          </button>
-        
+            <FormGroup>
+              <label>Description</label>
+              <Input
+                type="text"
+                value={this.state.description}
+                onChange={event =>
+                  this.setState({ description: event.target.value })
+                }
+              />
+            </FormGroup>
+            <Button color="primary">Submit</Button>
+          </Form>
+        </Container>
       </div>
     );
   }
